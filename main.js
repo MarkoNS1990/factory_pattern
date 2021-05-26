58fa1c0decaa9cc220f696bc9cba86375bbecd81
Marko Zecevic <marczecevic@gmail.com>
2021-05-26 11:44:04 +0200

    Add scope issues with var

--- main.js
+++ main.js
@@ -17,3 +17,26 @@ const age = 30
 const color = 'red'
 
 console.log({name,age,color})
+
+// Scope
+// Var is functionaly scoped ; let and const are block scoped
+let a = 17;
+
+const func = x => {
+    let a = x //Creating new variable in function scope, doesnt affect outer variable
+ 
+};
+
+func(99);
+
+console.log(a)
+
+
+var years = 10
+
+if (years > 5){
+      var doubleYears = years * 2 //it will be in global function scope
+    // const doubleYears = years * 2 //it wont be in global function scope as its in a block
+}
+
+console.log(doubleYears)
